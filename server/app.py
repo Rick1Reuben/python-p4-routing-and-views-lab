eedaf955ec90212e03dac130bfd4614583dbac38
Reuben Kamau <rickr6129@gmail.com>
2024-06-28 10:29:35 +0300

    Completed Lab

--- server/app.py
+++ server/app.py
@@ -6,3 +6,29 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<parameter>')
+def print_route(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    return '\n'.join(map(str, range(parameter))) + '\n'
+
+@app.route('/math/<int:a>/<op>/<int:b>')
+def math(a, op, b):
+    if op == '+':
+        return str(a + b)
+    elif op == '-':
+        return str(a - b)
+    elif op == '*':
+        return str(a * b)
+    elif op == 'div':
+        return str(a / b)
+    elif op == '%':
+        return str(a % b)
